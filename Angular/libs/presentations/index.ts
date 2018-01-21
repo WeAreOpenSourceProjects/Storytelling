eefce53da51ef37bac7575cfb571d41a4bccfc49
Foad Ardalan <foad.ardalan@outlook.fr>
2018-01-21 12:42:59 +0100

    clean

--- Angular/libs/presentations/index.ts
+++ Angular/libs/presentations/index.ts
@@ -1,4 +1,4 @@
 export { PresentationsModule } from './src/presentations.module'
 export { presentationsConfiguration } from './src/presentations.configuration';
 export { PresentationsListComponent } from './src/containers/presentations-list/presentations-list.component';
-export { PresentationDetailComponent } from './src/containers/presentation-detail/presentation-detail.component';
+export { PresentationEditComponent } from './src/containers/presentation-edit/presentation-edit.component';
