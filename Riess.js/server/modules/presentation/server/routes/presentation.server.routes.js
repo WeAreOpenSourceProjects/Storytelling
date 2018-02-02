013f942dba500d2eb8e584ee9a86c1249030e804
Foad Ardalan <foad.ardalan@outlook.fr>
2018-02-02 15:00:12 +0100

    drop sessions from server

--- Riess.js/server/modules/presentation/server/routes/presentation.server.routes.js
+++ Riess.js/server/modules/presentation/server/routes/presentation.server.routes.js
@@ -8,19 +8,19 @@ module.exports = function(app) {
 
   app.route('/api/presentations/search')
 //  .all(presentationPolicy.isAllowed)
-  .get(passport.authenticate('jwt'), presentation.search);
+  .get(presentation.search);
 
   app.route('/api/presentations/copy')
   //  .all(presentationPolicy.isAllowed)
-  .post(passport.authenticate('jwt'), presentation.copy);
+  .post(passport.authenticate('jwt'), passport.authenticate('jwt'), presentation.copy);
 
   app.route('/api/presentations/:presentationId')
 //  .all(presentationPolicy.isAllowed)
   .get(presentation.findOneById)
-  .patch(presentation.update)
-  .delete(presentation.delete);
+  .patch(passport.authenticate('jwt'), presentation.update)
+  .delete(passport.authenticate('jwt'), presentation.delete);
 
   app.route('/api/presentations')
   //  .all(presentationPolicy.isAllowed)
-  .post(presentation.create);
+  .post(passport.authenticate('jwt'), presentation.create);
 };
